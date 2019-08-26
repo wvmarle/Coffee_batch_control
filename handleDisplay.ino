28405ab8231d0ff383fac4c085bc8c821eba6a89
wvmarle <wouter@squirrel-systems.com>
2019-08-26 18:40:29 +0800

    Leading zero for discharge countdown seconds.

--- handleDisplay.ino
+++ handleDisplay.ino
@@ -58,7 +58,7 @@ void handleDisplay() {
         uint32_t timeToGo = BATCH_DISCHARGE_TIME - timePassed;
         uint8_t minutes = timeToGo / 60000ul;
         uint8_t seconds = uint32_t(timeToGo / 1000ul) % 60;
-        sprintf_P(linebuff, PSTR("%4i kg, #%i/%i %2u:%2u"), scaleWeight, nBatch, nBatches, minutes, seconds);
+        sprintf_P(linebuff, PSTR("%4i kg, #%i/%i %2u:%02u"), scaleWeight, nBatch, nBatches, minutes, seconds);
         break;
 
       case STOPPED:
